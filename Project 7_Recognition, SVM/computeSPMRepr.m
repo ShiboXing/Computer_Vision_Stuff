b4e2fee4e52632d32defbaaf28e17b28b869f786
ShiboXing <shx26@pitt.edu>
2019-10-28 15:23:21 -0400

    added recognition

---
+++ Project 7_Recognition, SVM/computeSPMRepr.m
@@ -0,0 +1,4 @@
+ function [pyramid, level_0, level_1, level_2] = computeSPMRepr(sift, means);
+ 
+ 
+ end
---
+++ Project 7_Recognition, SVM/test.m
@@ -0,0 +1,3 @@
+
+im=imread('scenes_train\scenes_train\coast\image_0100.jpg');
+load('scenes_train\scenes_train\coast\image_0100.jpg.mat');
