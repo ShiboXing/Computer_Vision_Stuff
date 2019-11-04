0725f22d8f32d620f0ce6855e628fe7f50166159
ShiboXing <shx26@pitt.edu>
2019-11-04 14:08:26 -0500

    generated pyramids' concatenation

--- Project 7_Recognition, SVM/findLabelsKNN.m
+++ Project 7_Recognition, SVM/findLabelsKNN.m
@@ -1,7 +1,5 @@
 function [predicted_labels_test] = findLabelsKNN(pyramids_train, labels_train, pyramids_test, k)
     dists=pdist2(pyramids_test,pyramids_train);
     
- 
-
 
 end
--- Project 7_Recognition, SVM/test.m
+++ Project 7_Recognition, SVM/test.m
@@ -3,3 +3,32 @@ sift=load('scenes_train\scenes_train\coast\image_0003.jpg.mat');
 means=load('means.mat');
 [P,A,B,C]=computeSPMRepr(sift,means.means);
 
+%fetch six sifts from train
+root='scenes_train/scenes_train/';
+dirs={'coast','forest','highway','insidecity','mountain','opencountry','street','tallbuilding'};
+pyramids_train=build_pyramids(root,dirs,means,1);
+size(pyramids_train)
+save('pyramids_train.mat','pyramids_train');
+root='scenes_test/scenes_test/';
+dirs={'coast','forest','highway','insidecity','mountain','opencountry','street','tallbuilding'};
+pyramids_test=build_pyramids(root,dirs,means,1);
+size(pyramids_test)
+save('pyramids_test.mat','pyramids_test');
+
+function [pyramids_train]=build_pyramids(root,dirs,means,num)
+    %concatenate all the selected descriptors
+    pyramids_train=cell(1);
+    for i=1:length(dirs)
+        path=char(strcat(root,dirs(i)));
+        S=dir(fullfile(path,'*.mat'));
+        for k = 1:min(numel(S))
+            F = fullfile(path,S(k).name);
+            sift= load(F);
+            [P,~,~,~]=computeSPMRepr(sift,means.means);
+            pyramids_train{(i-1)*numel(S)+k,1}=P;
+        end
+    end
+end
+
+
+
