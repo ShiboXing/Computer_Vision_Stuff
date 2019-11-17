9927d2dd7f7d9ee467d317de3b988392d1d4ea17
ShiboXing <shx26@pitt.edu>
2019-11-17 00:28:45 -0500

    updated losses

--- Project 8_Convolutional_Neutral_Network/cross_entropy_loss.m
+++ Project 8_Convolutional_Neutral_Network/cross_entropy_loss.m
@@ -1,4 +1,10 @@
  function [loss] = cross_entropy_loss(scores, correct_class)
- 
+    loss=0;
+    denom=0;
+    for j=1:size(scores,1)
+        denom=denom+exp(scores(j));
+    end
+    
+    loss=-log(exp(scores(correct_class))/denom);
  
  end
--- Project 8_Convolutional_Neutral_Network/losses.m
+++ Project 8_Convolutional_Neutral_Network/losses.m
@@ -5,11 +5,14 @@ inputs={x1 x2 x3 x4};
 
 
 for i =1:length(weights)
-    L=0;
+    L_h=0;%hinge loss
+    L_e=0;%cross entropy loss
     for j=1:length(inputs)
         scores=weights{i}*inputs{j};
-        L=L+hinge_loss(scores,j);
+        L_h=L_h+hinge_loss(scores,j);
+        L_e=L_e+cross_entropy_loss(scores,j);
     end
-    L=L/length(inputs);
+    L_h=L_h/length(inputs);
+    
 end
 
