84f8886c5324e3db8d269a855f22a4564e95601e
ShiboXing <shx26@pitt.edu>
2019-11-25 16:12:11 -0500

    completed CNNconstruction and training

---
+++ Project 9_Deep_Learning, AlexNet/part_i.m
@@ -0,0 +1,35 @@
+%I1
+f=fullfile('scenes_all','scenes_all');
+imds=imageDatastore(f,'IncludeSubfolders',true,'LabelSource','foldernames')
+NumOfImgs=100;
+[imdsTrain,imdsTest] = splitEachLabel(imds,NumOfImgs,'randomize');
+
+%I2
+layers =[%input layer
+    imageInputLayer([227 227 3])
+    %Group A layers
+    convolution2dLayer(11,50)
+    reluLayer
+    maxPooling2dLayer(3,'Stride',1)
+    %Group B layers
+    convolution2dLayer(5,60)
+    reluLayer
+    maxPooling2dLayer(3,'Stride',2)
+    fullyConnectedLayer(8)
+    softmaxLayer
+    classificationLayer];
+
+%I3
+options = trainingOptions('sgdm', ...
+    'MaxEpochs',1,...
+    'InitialLearnRate',1e-3, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+net = trainNetwork(imdsTrain,layers,options);
+
+    
+    
+
+
+
