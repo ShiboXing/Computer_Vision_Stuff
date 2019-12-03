e4d3f123808322bd4f81892877caacc22698ec92
Xing <shx26@scs-dlh230-m22.labs.pitt.edu>
2019-12-03 13:43:06 -0500

    completed training scripts, added gitignore

--- Project 9_Deep_Learning, AlexNet/part_iv.m
+++ Project 9_Deep_Learning, AlexNet/part_iv.m
@@ -9,15 +9,38 @@ net=alexnet;
 freezed_layers=net.Layers(1:22)
 layers=[freezed_layers;fullyConnectedLayer(8);softmaxLayer;classificationLayer];
 
-%training options
+%training options with learning rate as 1e-4
 options = trainingOptions('sgdm', ...
     'MaxEpochs',1,...
-    'InitialLearnRate',1e-3, ...
+    'InitialLearnRate',1e-4, ...
     'Verbose',false, ...
     'Plots','training-progress');
 
 NewNet = trainNetwork(imdsTrain,layers,options);
-save('alexnetTillFC6.mat');
+%save('alexnetTillFC6.mat');
+
+YPred = classify(NewNet,imdsTest);
+YTest = imdsTest.Labels;
+    
+%calculate performance
+correct=0.0;
+for i=1:size(YPred,1)
+    if (YPred(i)==YTest(i))
+        correct=correct+1;
+    end
+end
+ratio=correct/size(YPred,1)
+
+
+%training options with learning rate as 1e-2
+options = trainingOptions('sgdm', ...
+    'MaxEpochs',1,...
+    'InitialLearnRate',1e-2, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+NewNet = trainNetwork(imdsTrain,layers,options);
+%save('alexnetTillFC6.mat');
 
 YPred = classify(NewNet,imdsTest);
 YTest = imdsTest.Labels;
